3d8fa9d22020fba2cdcb1e99eca6e81ce6dc4589
taknaja <peerapolkoeram@gmail.com>
2025-09-19 21:43:28 +0700

    init project

---
+++ src/main/java/takee/dev/report/SpringbootGenerateReportApplication.java
@@ -0,0 +1,13 @@
+package takee.dev.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGenerateReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGenerateReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/takee/dev/report/SpringbootGenerateReportApplicationTests.java
@@ -0,0 +1,13 @@
+package takee.dev.report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGenerateReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
