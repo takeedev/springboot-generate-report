844306a40918c31baffcb3809318f847c70d8db2
taknaja <peerapolkoeram@gmail.com>
2025-10-10 21:25:59 +0700

    add ReportController

---
+++ src/main/java/takee/dev/report/controller/ReportController.java
@@ -0,0 +1,24 @@
+package takee.dev.report.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import takee.dev.report.entity.Reports;
+import takee.dev.report.service.ReportService;
+
+@RestController
+@RequiredArgsConstructor
+public class ReportController {
+
+    private final ReportService reportService;
+
+    @PostMapping("/add-report")
+    public ResponseEntity<String> addReport(@RequestBody Reports req) {
+        var result = reportService.saveReport(req);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+    }
+
+}
