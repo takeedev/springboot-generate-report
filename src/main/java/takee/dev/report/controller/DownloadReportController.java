0bdb942151ef0527ea2f4e3ac9972b82d774ffb4
Takee Dev <peerapolkoeram@gmail.com>
2026-02-06 21:18:03 +0700

    move path generation report to DownloadReportController

--- src/main/java/takee/dev/report/controller/DownloadReportController.java
+++ src/main/java/takee/dev/report/controller/DownloadReportController.java
@@ -1,14 +1,32 @@
 package takee.dev.report.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import takee.dev.report.common.dto.GeneratedFile;
+import takee.dev.report.service.DownloadReportServiceImp;
+import takee.dev.report.utils.ResponseExportFile;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "api/download-file")
 public class DownloadReportController {
 
+    private final DownloadReportServiceImp downloadReportServiceImp;
+    private final ResponseExportFile responseExportFile;
 
+    @GetMapping("/gen-normal-report")
+    public ResponseEntity<byte[]> generateNormalReport(String reportNo) {
+        GeneratedFile result = downloadReportServiceImp.genByReport(reportNo);
+        return responseExportFile.toResponseEntity(result);
+    }
+
+    @GetMapping("/gen-large-report")
+    public ResponseEntity<byte[]> generateLargeReport(String reportNo) {
+        GeneratedFile result = downloadReportServiceImp.genByReportLargeFiles(reportNo);
+        return responseExportFile.toResponseEntity(result);
+    }
 
 }
--- src/main/java/takee/dev/report/controller/ManageReportController.java
+++ src/main/java/takee/dev/report/controller/ManageReportController.java
@@ -9,12 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import takee.dev.report.common.dto.GeneratedFile;
 import takee.dev.report.entity.DailyReport;
 import takee.dev.report.entity.Reports;
-import takee.dev.report.service.DownloadReportServiceImp;
 import takee.dev.report.service.ManageReportServiceImp;
-import takee.dev.report.utils.ResponseExportFile;
 
 import java.util.List;
 
@@ -25,8 +22,6 @@ import java.util.List;
 public class ManageReportController {
 
     private final ManageReportServiceImp manageReportService;
-    private final DownloadReportServiceImp downloadReportServiceImp;
-    private final ResponseExportFile responseExportFile;
 
     @PostMapping("/add-report")
     public ResponseEntity<String> addReport(@RequestBody Reports req) {
@@ -52,15 +47,4 @@ public class ManageReportController {
         return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
 
-    @GetMapping("/gen-normal-report")
-    public ResponseEntity<byte[]> generateNormalReport(String reportNo) {
-        GeneratedFile result = downloadReportServiceImp.genByReport(reportNo);
-        return responseExportFile.toResponseEntity(result);
-    }
-
-    @GetMapping("/gen-large-report")
-    public ResponseEntity<byte[]> generateLargeReport(String reportNo) {
-        GeneratedFile result = downloadReportServiceImp.genByReportLargeFiles(reportNo);
-        return responseExportFile.toResponseEntity(result);
-    }
 }
